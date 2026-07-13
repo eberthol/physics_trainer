e43db65086b00c3d452902071369f88519c18d4e
Emilie Bertholet <emilie.bertholet@cern.ch>
2026-07-13 15:36:28 +0300

    more advanced, version controlled, service worker

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,102 @@
-const CACHE_NAME = "nuclide-v2";
+const CACHE_NAME = "nuclide-cache";
+const VERSION_KEY = "__version__";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+async function getLatestVersion() {
 
-  self.skipWaiting();
-});
+    const response = await fetch(
+        "version.json",
+        { cache: "no-store" }
+    );
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    return await response.json();
+}
 
-  self.clients.claim();
-});
+async function cacheVersion(cache, version){
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
-  );
-});
+    await cache.put(
+        VERSION_KEY,
+        new Response(version)
+    );
+
+}
+
+async function currentVersion(cache){
+
+    const response = await cache.match(VERSION_KEY);
+
+    if(!response)
+        return null;
+
+    return await response.text();
+
+}
+
+async function updateCache(cache){
+
+    await cache.addAll(FILES);
+
+}
+
+async function synchronize(){
+
+    const latest = await getLatestVersion();
+
+    const cache = await caches.open(CACHE_NAME);
+
+    const current = await currentVersion(cache);
+
+    if(current === latest.version){
+
+        console.log(
+            "NUCLIDE already up to date."
+        );
+
+        return;
+
+    }
+
+    console.log(
+        `Updating ${current} → ${latest.version}`
+    );
+
+    await updateCache(cache);
+
+    await cacheVersion(
+        cache,
+        latest.version
+    );
+
+}
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+            synchronize()
+        );
+
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(r => r || fetch(event.request))
+
+        );
+
+    }
+);
